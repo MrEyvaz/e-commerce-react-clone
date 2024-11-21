e34ece2503fd82d7a37d9a57945342843fa3dc85
MrEyvaz <eyvaz.aliyev.7740@gmail.com>
2024-11-21 14:46:11 +0400

    e-commerce

--- src/components/wishlist/WishList.jsx
+++ src/components/wishlist/WishList.jsx
@@ -18,7 +18,7 @@ function WishList() {
     const navigate = useNavigate()
 
     const justForYouProductIds = ["RmyjkFh9Jcsz8S2UhyIR", "ZIC2X60QpA1CSaXjiPG5", "MMgYqXGkmSBjITlQkrjO", "hKR9VYr7oAZVZgBzAjGz"]
-    const justForYouProducts = products.filter((product) => justForYouProductIds.includes(product.id))
+    const justForYouProducts = products?.length > 0 ? products.filter((product) => justForYouProductIds.includes(product.id)) : [];
 
     const handleProductClick = (productId) => {
         navigate(`/product/${productId}`)
@@ -27,7 +27,7 @@ function WishList() {
     favorites.forEach((fav) => {
         console.log("Favorite item:", fav);
     });
-    
+
     return (
         <div>
             {/* Sale and language selector */}
@@ -39,28 +39,30 @@ function WishList() {
 
             <h3 className={styles.wishListCountTitle}>{`Wishlist (${favorites.length})`}</h3>
             <ul className={styles.wishListItems}>
-                {favorites.length > 0 ? (
-                    favorites.map((fav) => (
-                        <li key={fav.id} className={styles.wishListItem}>
-                            <img src={fav.item.image} alt={fav.item.title} />
-                            <h5>{fav.item.title}</h5>
-                            <span style={{ color: "rgb(219,68,68)", fontSize: "18px" }}>{fav.item.price}</span>
-                            <span style={{ marginLeft: "24px", textDecoration: "line-through", fontSize: "18px" }}>{fav.item.discountPrice}</span>
-                            <div className={styles.wishListActions}>
-                                <button className='btn btn-dark' onClick={() => addToCart(fav.item)}>Add to Cart</button>
-                                {favorites.some((favorite) => favorite.id === fav.id) ? (
-                                    <FavoriteIcon style={{ color: "red" }} onClick={() => toggleFavorite(fav)} />
-                                ) : (
-                                    <FavoriteBorderIcon onClick={() => toggleFavorite(fav)} />
-                                )}
-                            </div>
-                        </li>
-                    ))
-                ) : (
-                    <div className={styles.noFountTitle}>
-                        <p>No items in your wishlist</p>
-                    </div>
-                )}
+                {favorites.map((fav) => (
+                    <li key={fav.id} className={styles.wishListItem}>
+                        {fav.item ? (
+                            <>
+                                <img src={fav.item.image} alt={fav.item.title} />
+                                <h5>{fav.item.title}</h5>
+                                <span style={{ color: "rgb(219,68,68)", fontSize: "18px" }}>{fav.item.price}</span>
+                                <span style={{ marginLeft: "24px", textDecoration: "line-through", fontSize: "18px" }}>{fav.item.discountPrice}</span>
+                                <div className={styles.wishListActions}>
+                                    <button className='btn btn-dark' onClick={() => addToCart(fav.item)}>Add to Cart</button>
+                                    {favorites.includes(fav) ? (
+                                        <FavoriteIcon style={{ color: "red" }} onClick={() => toggleFavorite(fav)} />
+                                    ) : (
+                                        <FavoriteBorderIcon onClick={() => toggleFavorite(fav)} />
+                                    )}
+
+                                </div>
+                            </>
+                        ) : (
+                            <p>Invalid item</p>
+                        )}
+                    </li>
+                ))}
+
             </ul>
 
             <div className={styles.wishListVertical}></div>
